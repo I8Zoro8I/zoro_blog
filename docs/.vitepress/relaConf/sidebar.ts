c4ca9f9ed8c5ee305a38a1fa199754fb1c4b0da1
chenyx <chenyx@solot.com>
2026-05-06 13:49:05 +0800

    deploy

--- docs/.vitepress/relaConf/sidebar.ts
+++ docs/.vitepress/relaConf/sidebar.ts
@@ -1,47 +1,72 @@
-import {DefaultTheme} from 'vitepress';
+// import {DefaultTheme} from 'vitepress';
+//
+// export const sidebar: DefaultTheme.Sidebar = {
+//
+//     // /column/Algothm/表示对这个文件夹下的所有md文件做侧边栏配置
+//     '/column/Algorithm/': [
+//         {
+//
+//             text: '首页',
+//             link: '/column/Algorithm/index'
+//         },
+//         // 第一部分
+//         {
+//
+//             text: '栈和队列',
+//             items: [
+//                 {
+//
+//                     text: '栈-深拷贝和浅拷贝',
+//                     link: '/column/Algorithm/001_Stack'
+//                 },
+//                 {
+//
+//                     text: '队列-事件循环',
+//                     link: '/column/Algorithm/002_Queue'
+//                 }
+//             ]
+//         },
+//         // 第二部分
+//         {
+//
+//             text: '字典和树',
+//             items: [
+//                 {
+//
+//                     text: '字典和集合-Set和Map',
+//                     link: '/column/Algorithm/003_Dictionary'
+//                 },
+//                 {
+//
+//                     text: '树-深/广度优先遍历',
+//                     link: '/column/Algorithm/004_Tree'
+//                 }
+//             ]
+//         }
+//     ]
+// };
 
-export const sidebar: DefaultTheme.Sidebar = {
+import { DefaultTheme } from 'vitepress';
+import { categories } from './categories';
 
-    // /column/Algothm/表示对这个文件夹下的所有md文件做侧边栏配置
-    '/column/Algorithm/': [
-        {
+export const sidebar: DefaultTheme.Sidebar = {};
 
-            text: '首页',
-            link: '/column/Algorithm/index'
-        },
-        // 第一部分
-        {
+// 将 JSON 数据转换为 VitePress 要求的格式
+categories.forEach((cat: any) => {
+    const sidebarKey = `/column/${cat.id}/`;
 
-            text: '栈和队列',
-            items: [
-                {
-
-                    text: '栈-深拷贝和浅拷贝',
-                    link: '/column/Algorithm/001_Stack'
-                },
-                {
-
-                    text: '队列-事件循环',
-                    link: '/column/Algorithm/002_Queue'
-                }
-            ]
-        },
-        // 第二部分
-        {
-
-            text: '字典和树',
-            items: [
-                {
-
-                    text: '字典和集合-Set和Map',
-                    link: '/column/Algorithm/003_Dictionary'
-                },
-                {
-
-                    text: '树-深/广度优先遍历',
-                    link: '/column/Algorithm/004_Tree'
-                }
-            ]
-        }
-    ]
-};
+    // 生成该分类下的数组
+    sidebar[sidebarKey] = [
+        // 自动添加一个首页
+        { text: `${cat.title}首页`, link: `${sidebarKey}index` },
+        // 循环 JSON 中的分组
+        ...cat.groups.map((group: any) => ({
+            text: group.groupName,
+            collapsed: false,
+            items: group.items.map((item: any) => ({
+                text: item.text,
+                link: item.link
+            }))
+        }))
+    ];
+});
